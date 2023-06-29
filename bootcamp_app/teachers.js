6431bac48c57d565c638c636b9a90b567e8b205b
Alice Lin <alicexxlin@gmail.com>
2023-06-29 03:21:43 +0000

    implement parameterized query

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -10,15 +10,19 @@ const pool = new Pool({
 console.log('connected');
 const cohortName = process.argv[2];
 
-pool.query(`
-SELECT DISTINCT cohorts.name, teachers.name as teacher
-FROM assistance_requests
-JOIN teachers ON teachers.id = teacher_id
-JOIN students ON students.id = student_id
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name = '${cohortName}'
-ORDER BY teachers.name
-`)
+const value = [cohortName];
+
+const queryString = `
+  SELECT DISTINCT cohorts.name, teachers.name as teacher
+  FROM assistance_requests
+  JOIN teachers ON teachers.id = teacher_id
+  JOIN students ON students.id = student_id
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name = $1
+  ORDER BY teachers.name
+`;
+
+pool.query(queryString, value)
   .then(res => {
     res.rows.forEach(row => {
       console.log(`${cohortName}: ${row.teacher}`);
